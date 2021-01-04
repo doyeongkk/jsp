cba7b301f16b12353fa2e89d1b9c5145288f9c71
jwlee <doyeongk99@gmail.com>
2021-01-04 13:27:41 +0900

    jsp 기본객체  . scope 객체  . HTTP프로토콜의 사용자 식별 프로세스
    scope 객체 실습

---
+++ src/main/java/kr/or/ddit/servlet/sumCalculation/sumCalculation.java
@@ -0,0 +1,49 @@
+package kr.or.ddit.servlet.sumCalculation;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/sumCalculation")
+public class sumCalculation extends HttpServlet {
+    
+
+	
+		
+		@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			req.getRequestDispatcher("/jsp/sum.jsp").forward(req, resp);
+		}
+		
+		@Override
+		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			int sumParameter = Integer.parseInt(req.getParameter("start"));
+			int sumParameter2 = Integer.parseInt(req.getParameter("end"));
+			
+			int result = 0;
+			
+			for(int i =sumParameter; i<= sumParameter2; i++) {
+				result += i;
+			}
+		
+	    HttpSession session = req.getSession();
+	    session.setAttribute("session", result);
+		
+	    req.getRequestDispatcher("/jsp/sumResult.jsp").forward(req, resp);
+		}
+	}
+
+
+
+
+
+
+
+
+
